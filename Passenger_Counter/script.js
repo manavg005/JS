c1b4b6819133f13bf025f9384d1b99f86e2d62e9
Manav <manavguptamg1990@gmail.com>
2021-06-14 16:54:50 +0530

    JS1

---
+++ Passenger_Counter/script.js
@@ -0,0 +1,10 @@
+let countEl = document.getElementById("el-count")
+console.log(countEL);
+
+let count=0
+
+function increment() {
+    console.log("The button was clicked")
+    count=count+1
+    console.log(count);
+}
